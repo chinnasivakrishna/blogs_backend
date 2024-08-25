607c5da5915fbc4e2d87ac0a945a38609a1fe4a3
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-08-26 01:05:07 +0530

    Update blogRoutes.js

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -93,10 +93,8 @@ blog.route('/:blogId/comments').get(async (req, res) => {
 
   try {
     const blog = await Blog.findById(blogId).select('comments');
-    console.log(blog.user)
-    const Email = blog.user
-    const name = await User.findOne({ Email });
-    console.log(name)
+    const user = await Blog.findById(blogId);
+    console.log(user)
     if (!blog) return res.status(404).json({ message: 'Blog not found' });
 
     res.status(200).json(blog.comments);
